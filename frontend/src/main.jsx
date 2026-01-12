0c628dfaeed8ebd39eb6c05e5c71a4496764a345
Shoe-lucas <lucasmatosplaygo@gmail.com>
2026-01-12 19:16:31 -0300

    main concertado

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/global.scss";
+import "./components/styles/global.scss"
 import Home from "./components/pages/home";
 
 createRoot(document.getElementById("root")).render(
